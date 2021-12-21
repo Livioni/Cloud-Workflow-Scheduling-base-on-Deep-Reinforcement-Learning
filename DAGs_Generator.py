4e929759586571ee9d71073c931e869f707958bb
Livioni <livion_i@icloud.com>
2021-12-21 17:20:48 +0800

    更新Workflow generator
    新添功能文件，拟在gym中构建强化学习环境

--- DAGs_Generator.py
+++ DAGs_Generator.py
@@ -1,93 +1,138 @@
 import random,math,argparse
 import numpy as np
 from numpy.random.mtrand import sample
+from matplotlib import pyplot as plt
+import networkx as nx
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mode', default='default', type=str)#参数定义
-parser.add_argument('--n', default=20, type=int)#节点数量
-parser.add_argument('--prob', default=0.2, type=float)#跳跃概率
-parser.add_argument('--max_out', default=2, type=float)#最大出口
-parser.add_argument('--alpha',default=1,type=float) #shape
-parser.add_argument('--beta',default=1.0,type=float)  #规则度
-parser.add_argument('--jump',default=1,type=int)     #DAG 跳跃层
+parser.add_argument('--mode', default='default', type=str)#parameters setting
+parser.add_argument('--n', default=10, type=int)          #number of DAG  nodes
+parser.add_argument('--max_out', default=2, type=float)   #max out_degree of one node
+parser.add_argument('--alpha',default=1,type=float)       #shape 
+parser.add_argument('--beta',default=1.0,type=float)      #regularity
 args = parser.parse_args()
 
-set_dag_size = [20,30,40,50,60,70,80,90,100]         #随机DAG数量       
-set_max_out = [2,3,4,5]                              #最大出节点数
-set_alpha = [0.5,1.0,2.0]                            #DAG shape
-set_beta = [0.0,0.5,1.0,2.0]                         #DAG 规则度
-set_jump = [1,2,3]                                   #DAG 跳跃层
+set_dag_size = [20,30,40,50,60,70,80,90]             #random number of DAG  nodes       
+set_max_out = [1,2,3,4,5]                              #max out_degree of one node
+set_alpha = [0.5,1.0,1.5]                            #DAG shape
+set_beta = [0.0,0.5,1.0,2.0]                         #DAG regularity
 
-##############################################initialize############################################
-if args.mode != 'default':
-    args.n = random.sample(set_dag_size,1)[0]
-    args.max_out = random.sample(set_max_out,1)[0]
-    args.alpha = random.sample(set_alpha,1)[0]
-    args.beta = random.sample(set_alpha,1)[0]
+def DAGs_generate(mode = 'default', n = 10, max_out = 2,alpha = 1,beta = 1.0):
+    ##############################################initialize############################################
+    if mode != 'default':
+        args.n = random.sample(set_dag_size,1)[0]
+        args.max_out = random.sample(set_max_out,1)[0]
+        args.alpha = random.sample(set_alpha,1)[0]
+        args.beta = random.sample(set_alpha,1)[0]
+    else: 
+        args.n = n
+        args.max_out = max_out
+        args.alpha = alpha
+        args.beta = beta
 
-length = math.floor(math.sqrt(args.n)/args.alpha)
-mean_value = args.n/length
-random_num = np.random.normal(loc = mean_value, scale = args.beta,  size = (length,1))    #生成随机正态分布数
-###############################################division#############################################
+    length = math.floor(math.sqrt(args.n)/args.alpha)
+    mean_value = args.n/length
+    random_num = np.random.normal(loc = mean_value, scale = args.beta,  size = (length,1))    
+    ###############################################division#############################################
+    position = {'Start':(0,4),'Exit':(10,4)}
+    generate_num = 0
+    dag_num = 1
+    dag_list = [] 
+    for i in range(len(random_num)):
+        dag_list.append([]) 
+        for j in range(math.ceil(random_num[i])):
+            dag_list[i].append(j)
+        generate_num += math.ceil(random_num[i])
 
-generate_num = 0
-dag_num = 1
-dag_list = [] 
-for i in range(len(random_num)):
-    dag_list.append([]) 
-    for j in range(math.ceil(random_num[i])):
-        dag_list[i].append(j)
-    generate_num += math.ceil(random_num[i])
+    if generate_num != args.n:
+        if generate_num<args.n:
+            for i in range(args.n-generate_num):
+                index = random.randrange(0,length,1)
+                dag_list[index].append(len(dag_list[index]))
+        if generate_num>args.n:
+            i = 0
+            while i < generate_num-args.n:
+                index = random.randrange(0,length,1)
+                if len(dag_list[index])==1:
+                    i = i-1 if i!=0 else 0
+                else:
+                    del dag_list[index][-1]
+                i += 1
 
-if generate_num != args.n:
-    if generate_num<args.n:
-        for i in range(args.n-generate_num):
-            index = random.randrange(0,length,1)
-            dag_list[index].append(len(dag_list[index]))
-    if generate_num>args.n:
-        i = 0
-        while i < generate_num-args.n:
-            index = random.randrange(0,length,1)
-            if len(dag_list[index])==1:
-                i = i-1 if i!=0 else 0
-            else:
-                del dag_list[index][-1]
-            i += 1
+    dag_list_update = []
+    pos = 1
+    max_pos = 0
+    for i in range(length):
+        dag_list_update.append(list(range(dag_num,dag_num+len(dag_list[i]))))
+        dag_num += len(dag_list_update[i])
+        pos = 1
+        for j in dag_list_update[i]:
+            position[j] = (3*(i+1),pos)
+            pos += 5
+        max_pos = pos if pos > max_pos else max_pos
+        position['Start']=(0,max_pos/2)
+        position['Exit']=(3*(length+1),max_pos/2)
 
-dag_list_update = []
-for i in range(length):
-    dag_list_update.append(list(range(dag_num,dag_num+len(dag_list[i]))))
-    dag_num += len(dag_list_update[i])
+    ############################################link#####################################################
+    into_degree = [0]*args.n            
+    out_degree = [0]*args.n             
+    edges = []                          
+    pred = 0
 
-############################################link#####################################################
-into_degree = [0]*args.n#节点入度列表
-out_degree = [0]*args.n#节点出度列表
-edges = []#存储边的列表
-pred = 0
-
-for i in range(length-1):
-    sample_list = list(range(len(dag_list_update[i+1])))
-    for j in range(len(dag_list_update[i])):
-        od = random.randrange(1,args.max_out+1,1)
-        bridge = random.sample(sample_list,od)
-        for k in bridge:
-            edges.append((dag_list_update[i][j],dag_list_update[i+1][k]))#连边
-            into_degree[pred+len(dag_list_update[i])+k]+=1
-            out_degree[pred+j]+=1 
-    pred += len(dag_list_update[i])
-
-
-######################################create start node and exit node################################
-for node,id in enumerate(into_degree):#给所有没有入边的节点添加入口节点作父亲
-    if id ==0:
-        edges.append((0,node+1))
-        into_degree[node]+=1
-
-for node,od in enumerate(out_degree):#给所有没有出边的节点添加出口节点作儿子
-    if od ==0:
-        edges.append((node+1,args.n+1))
-        out_degree[node]+=1
+    for i in range(length-1):
+        sample_list = list(range(len(dag_list_update[i+1])))
+        for j in range(len(dag_list_update[i])):
+            od = random.randrange(1,args.max_out+1,1)
+            od = len(dag_list_update[i+1]) if len(dag_list_update[i+1])<od else od
+            bridge = random.sample(sample_list,od)
+            for k in bridge:
+                edges.append((dag_list_update[i][j],dag_list_update[i+1][k]))
+                into_degree[pred+len(dag_list_update[i])+k]+=1
+                out_degree[pred+j]+=1 
+        pred += len(dag_list_update[i])
 
 
-print(edges)
-print(into_degree,out_degree)
+    ######################################create start node and exit node################################
+    for node,id in enumerate(into_degree):#给所有没有入边的节点添加入口节点作父亲
+        if id ==0:
+            edges.append(('Start',node+1))
+            into_degree[node]+=1
+
+    for node,od in enumerate(out_degree):#给所有没有出边的节点添加出口节点作儿子
+        if od ==0:
+            edges.append((node+1,'Exit'))
+            out_degree[node]+=1
+
+    #############################################plot###################################################
+    return edges,into_degree,out_degree,position
+
+def plot_DAG(edges,postion):
+    g1 = nx.DiGraph()
+    g1.add_edges_from(edges)
+    nx.draw_networkx(g1, arrows=True, pos=postion)
+    plt.savefig("DAG.png", format="PNG")
+    return plt.clf
+
+def workflows_generator(mode = 'default', n = 10, max_out = 2,alpha = 1,beta = 1.0, t_unit = 10, resource_unit = 100):
+    t = t_unit  #s   time unit
+    r = resource_unit #resource unit
+    edges,in_degree,out_degree,position = DAGs_generate(mode,n,max_out,alpha,beta)
+    plot_DAG(edges,position)
+    duration = []
+    demand = []
+    #初始化持续时间
+    for i in range(len(in_degree)):
+        if random.random()<0.8:
+            duration.append(random.uniform(t,3*t))
+        else:
+            duration.append(random.uniform(10*t,15*t))
+    #初始化资源需求   
+    for i in range(len(in_degree)):
+        if random.random()<0.5:
+            demand.append((random.uniform(0.25*r,0.5*r),random.uniform(0.05*r,0.01*r)))
+        else:
+            demand.append((random.uniform(0.05*r,0.01*r),random.uniform(0.25*r,0.5*r)))
+
+    return edges,duration,demand
+
+
--- playground.py
+++ playground.py
@@ -1,2 +1,7 @@
-dag = list(range(1,5))
-print(dag)
+from DAGs_Generator import DAGs_generate
+from DAGs_Generator import plot_DAG
+
+# edges,in_degree,out_degree,position = DAGs_generate('default')
+edges,in_degree,out_degree,position = DAGs_generate('default',n = 30,max_out = 3,alpha = 1.5,beta = 1.0)
+plot_DAG(edges,position)
+print(edges,in_degree,out_degree)
